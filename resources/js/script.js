52b278da38565098129cc8a4cf63d968695a3260
Carlos Daccarett <cdaccarett93@gmail.com>
2017-07-10 19:11:14 -0700

    intial website

---
+++ resources/js/script.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+        Typed.new('.element', {
+        strings: ["Computer Science", "Developer", "Innovator", "Creator"],
+        typeSpeed: 150,
+        loop: null
+      });
+    
+    $('.hamburger-shell').click(function(){
+		$('#menu').slideToggle(300);
+		$('.top').toggleClass('rotate');
+		$('.middle').toggleClass('rotate-back');
+        $('.bottom').toggleClass('bottom-active');
+		$('.menu-name').toggleClass('bump');
+		$('.bg-cover').toggleClass('reveal');
+	});
+	$('.bg-cover').click(function(){
+		$('#menu').slideToggle(300);
+		$('.top').toggleClass('rotate');
+		$('.middle').toggleClass('rotate-back');
+		$('.menu-name').toggleClass('bump');
+		$('.bg-cover').toggleClass('reveal');
+	});
+    /* Navigation */
+    // Select all links with hashes
+    $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function () {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
+        });
+
+});
+
+
+
---
+++ resources/js/threejsobject.js
@@ -0,0 +1,63 @@
+    var SCREEN_WIDTH = window.innerWidth,
+        SCREEN_HEIGHT = window.innerHeight,
+        windowHalfX = window.innerWidth / 2,
+        windowHalfY = window.innerHeight / 2;
+
+    var shape = (function () {
+        var scene = new THREE.Scene();
+        var camera = new THREE.PerspectiveCamera(75, SCREEN_WIDTH / SCREEN_HEIGHT, 0.1, 1000);
+
+        var renderer = new THREE.WebGLRenderer({ alpha: true } );
+        renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+        renderer.setClearColor (0x000000, 1);
+        document.getElementById("WebGL-output").appendChild(renderer.domElement);
+        
+        //LIGHTS
+        var lights = [];
+			lights[ 0 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+			lights[ 1 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+			lights[ 2 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+
+			lights[ 0 ].position.set( 0, 200, 0 );
+			lights[ 1 ].position.set( 100, 200, 100 );
+			lights[ 2 ].position.set( - 100, - 200, - 100 );
+
+			scene.add( lights[ 0 ] );
+			scene.add( lights[ 1 ] );
+			scene.add( lights[ 2 ] );        
+        
+//        var geometry = new THREE.BoxGeometry(1, 1, 1);
+        var geometry = new THREE.DodecahedronGeometry(2,0);
+        var material = new THREE.MeshPhongMaterial({
+            color: 0x57575A,
+            emissive: 0x151516,
+            side: THREE.DoubleSide,
+            shading: THREE.FlatShading
+        });
+        var cube = new THREE.Mesh(geometry, material);
+        
+        scene.add(cube);
+        cube.position.y = -0.8;
+
+        camera.position.z = 5;
+
+        var animate = function () {
+            requestAnimationFrame(animate);
+
+            cube.rotation.x += 0.003;
+            cube.rotation.y += 0.005;
+
+            renderer.render(scene, camera);
+        };
+
+        animate();
+        window.addEventListener('resize', onWindowResize, false);
+
+        function onWindowResize() {
+            windowHalfX = window.innerWidth / 2;
+            windowHalfY = window.innerHeight / 2;
+            camera.aspect = window.innerWidth / window.innerHeight;
+            camera.updateProjectionMatrix();
+            renderer.setSize(window.innerWidth, window.innerHeight);
+        }
+    })();
