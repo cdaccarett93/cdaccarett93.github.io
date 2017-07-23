1390dd2b9a53938df70482a36ef5a3ff2fa6c3bb
Carlos Daccarett <cdaccarett93@gmail.com>
2017-07-23 14:43:15 -0700

    three js geometry faster spin

--- resources/js/threejsobject.js
+++ resources/js/threejsobject.js
@@ -53,8 +53,8 @@
         var animate = function () {
             requestAnimationFrame(animate);
 
-            cube.rotation.x += 0.003;
-            cube.rotation.y += 0.005;
+            cube.rotation.x += 0.006;
+            cube.rotation.y += 0.008;
 
             renderer.render(scene, camera);
         };
